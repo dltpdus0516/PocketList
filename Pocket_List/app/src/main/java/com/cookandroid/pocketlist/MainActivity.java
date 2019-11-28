92d43a7bdb7d1bf046e91078eb536bcb1392b3a2
honghayeon <41175122+honghayeon@users.noreply.github.com>
2019-11-29 08:18:54 +0900

    activity_start

--- Pocket_List/app/src/main/java/com/cookandroid/pocketlist/MainActivity.java
+++ Pocket_List/app/src/main/java/com/cookandroid/pocketlist/MainActivity.java
@@ -9,5 +9,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
+=======
+
+        settingBtn = (ImageButton)findViewById(R.id.settingBtn);
+
+        // 환경설정 버튼 누르면 SettingActivity를 불러옴
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingIntent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(settingIntent);
+            }
+        });
+>>>>>>> Stashed changes
     }
 }
